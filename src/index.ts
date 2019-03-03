d1a106840d993f0c7e5863a240134187a37bc0a3
Ryo Ota <nwtgck@gmail.com>
2019-03-03 18:21:30 +0900

    [impl] Inject GA tracking code into HTML files

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as fs from "fs";
 import * as googleAnalyticsInjector from "./google-analytics-injector";
 import * as yargs from "yargs";
 import * as detectFileType from "detect-file-type";
+import * as path from "path";
 
 const parser = yargs
   .option("tracking-id", {
@@ -22,24 +23,29 @@ const args = parser.parse(argv);
 const trackingId: string = args["tracking-id"];
 
 // TODO: Parse properly
-const last = argv[argv.length - 1];
-const stat = fs.statSync(last);
-if (stat.isFile()) {
-  const filePath = last;
-  detectFileType.fromFile(filePath, (err: Error, filetype: any)=>{
-    // If file is HTML
-    if (filetype.mime === "text/html") {
-      const html = fs.readFileSync(filePath).toString("UTF-8");
-      // Get injected HTML
-      const injectedHtml = googleAnalyticsInjector.injectedHtml(html, trackingId);
-      // Rewrite injected HTML
-      fs.writeFileSync(filePath, injectedHtml);
-      console.log(`'${filePath}' is injected!`);
+const fileOrDirPath = argv[argv.length - 1];
+// Inject GA tracking code into HTML files
+injectRecursively(fileOrDirPath);
+
+function injectRecursively(fileOrDirPath: string): void {
+  const stat = fs.statSync(fileOrDirPath);
+  if (stat.isFile()) {
+    const filePath = fileOrDirPath;
+    detectFileType.fromFile(filePath, (err: Error, filetype: any)=>{
+      // If file is HTML
+      if (err === null && filetype !== null && filetype.mime === "text/html") {
+        const html = fs.readFileSync(filePath).toString("UTF-8");
+        // Get injected HTML
+        const injectedHtml = googleAnalyticsInjector.injectedHtml(html, trackingId);
+        // Rewrite injected HTML
+        fs.writeFileSync(filePath, injectedHtml);
+        console.log(`'${filePath}' is injected!`);
+      }
+    });
+  } else if (stat.isDirectory()) {
+    const filePaths = fs.readdirSync(fileOrDirPath);
+    for (const filePath of filePaths) {
+      injectRecursively(path.join(fileOrDirPath, filePath));
     }
-  });
-} else if (stat.isDirectory) {
-  // TODO: impl
-  console.log("Not implemented yet");
-} else {
-  console.log("NO")
+  }
 }
